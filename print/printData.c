1e4f18737582bf0cfc0f00263b82410ff3436490
hocine abderrahmane <95042350+Abderrahmanehocine@users.noreply.github.com>
2022-03-18 00:03:06 +0100

    Update printData.c

--- print/printData.c
+++ print/printData.c
@@ -1,11 +1,11 @@
 #include "printData.h"
 
-
+// Print all data in the binary search tree Inorder(from lesser to greater)
 void print(struct node* root){
   if(root == NULL){
     return;
   }
-  print(root->left);
-  printf("%d ",root->data);
-  print(root->right);
+  print(root->left); // recursive call to the left node
+  printf("%d ",root->data); prit data
+  print(root->right); // recursive call to the right node
 }
