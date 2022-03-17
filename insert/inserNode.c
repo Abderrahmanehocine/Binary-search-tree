ff251d5a13adc791657d1d84c4fa60c56e70785c
hocine abderrahmane <95042350+Abderrahmanehocine@users.noreply.github.com>
2022-03-17 23:56:48 +0100

    Update inserNode.c

--- insert/inserNode.c
+++ insert/inserNode.c
@@ -1,5 +1,6 @@
 #include "insertNode.h"
 
+// Create new node and insert data 
 struct node* NewNode(int data){
   struct node* node=(struct node*)malloc(sizeof(struct node));
   node->data=data;
@@ -7,6 +8,7 @@ struct node* NewNode(int data){
   return node;
 }
 
+// Manage node place in the binary search tree
 struct node* insert(struct node* root , int data){
   if(root == NULL){
     root=NewNode(data);
